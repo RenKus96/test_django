92ceaf62286ec6e64aa419d56f625acb4d102c19
RenKus <79753231+RenKus96@users.noreply.github.com>
2021-06-10 23:25:13 +0300

    Delete forms.py

--- students/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-
-from students.models import Student
-
-
-class StudentCreateForm(ModelForm):
-    class Meta:
-        model = Student
-        fields = ['first_name', 'last_name', 'age']
