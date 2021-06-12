1c5a32fd1b0d68510be41a71ed2b4330243a29a9
RenKus <79753231+RenKus96@users.noreply.github.com>
2021-06-13 01:34:13 +0300

    ДЗ

---
+++ teachers/management/commands/generate_teachers.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandError
+from teachers.models import Teacher
+
+class Command(BaseCommand):
+    help = 'Команда для генерация преподавателей'
+
+    def add_arguments(self, parser):
+        parser.add_argument('count', type=int, nargs='?', default=10, help=u'Количество создаваемых преподавателей')
+
+    def handle(self, *args, **options):
+        count = options['count']
+        out_str = f'Сгенерировано {count} преподавателей:\n'
+        for num, teacher in enumerate(Teacher.generate_teachers(count),1):
+            out_str += f'{num}. {teacher}\n'
+        self.stdout.write(self.style.SUCCESS(out_str))
