7557d87f7a86b00eaf88b8010d1bdfb38ce65fd1
RenKus <79753231+RenKus96@users.noreply.github.com>
2021-07-11 22:24:04 +0300

    HW2_14

--- core/models.py
+++ core/models.py
@@ -1,3 +1,57 @@
-from django.db import models
+import datetime
+from dateutil.relativedelta import relativedelta
 
-# Create your models here.
+from django.db import models
+from django.core.validators import MinLengthValidator
+
+from faker import Faker
+
+from students.validators import AdultValidator, email_stop_list_validator
+
+
+class Person(models.Model):
+    class Meta:
+        abstract = True
+
+    last_name = models.CharField(
+        max_length=80, null=False, validators=[MinLengthValidator(2)
+    ])
+    first_name = models.CharField(max_length=50, null=False)
+    age = models.IntegerField(default=42)
+    birthdate = models.DateField(
+        # default=datetime.date.today, validators=[adult_validator]
+        default=datetime.date.today, validators=[AdultValidator(21)]
+    )
+    email = models.EmailField(max_length=120, null=True, validators=[
+        email_stop_list_validator
+    ])
+    phone_number = models.CharField(max_length=17, blank=True, unique=True, null=True)
+
+    def __str__(self):
+        return f'{self.full_name()}, {self.birthdate}'
+
+    def full_name(self):
+        return f'{self.first_name}, {self.last_name}'
+
+    def save(self, *args, **kwargs):
+        self.age = relativedelta(datetime.date.today(), self.birthdate).years
+        super().save(*args, **kwargs)
+
+    @classmethod
+    def _generate(cls):
+        faker = Faker()
+        st = cls(
+            first_name=faker.first_name(),
+            last_name=faker.last_name(),
+            email=faker.email(),
+            birthdate=faker.date_between(start_date='-65y', end_date='-18y'),
+        )
+
+        st.age = relativedelta(datetime.date.today(), st.birthdate).years
+        st.save()
+        return st
+
+    @classmethod
+    def generate(cls, count):
+        for _ in range(count):
+            cls._generate()
--- teachers/models.py
+++ teachers/models.py
@@ -1,12 +1,15 @@
 import datetime
+from dateutil.relativedelta import relativedelta
 import random
 
 from django.db import models
 
 from faker import Faker
 
-from students.validators import email_stop_list_validator
+# from students.validators import email_stop_list_validator
 from groups.models import Group
+from core.models import Person
+
 
 ACADEMIC_DEGREES = [
     'ассистент', 'преподаватель', 'старший преподаватель',
@@ -15,24 +18,26 @@ ACADEMIC_DEGREES = [
 
 
 # Create your models here.
-class Teacher(models.Model):
-    last_name = models.CharField(max_length=80, null=False)
-    first_name = models.CharField(max_length=50, null=False)
-    birthdate = models.DateField(default=datetime.date.today)
-    email = models.EmailField(
-        max_length=120, null=True, validators=[email_stop_list_validator]
-    )
-    phone_number = models.CharField(max_length=17, blank=True, unique=True, null=True)
+class Teacher(Person):
     years_of_experience = models.IntegerField(default=0)
     academic_degrees = models.CharField(max_length=80, null=False)
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True, related_name='teachers')
 
     def __str__(self):
-        return f'{self.academic_degrees} {self.full_name()}, \
-            email:{self.email}'
+        return f'{self.academic_degrees} {self.full_name()}'
 
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
+
+
+    @classmethod
+    def _generate(cls):
+        faker = Faker()
+        obj = super()._generate()
+        obj.years_of_experience=faker.random_int(min=1, max=60),
+        obj.academic_degrees=random.choice(ACADEMIC_DEGREES),
+        obj.save()
+
 
     @staticmethod
     def generate_teachers(count):
@@ -42,14 +47,12 @@ class Teacher(models.Model):
             tchr = Teacher(
                 first_name=faker.first_name(),
                 last_name=faker.last_name(),
-                birthdate=faker.date_between(
-                    start_date='-80y',
-                    end_date='-20y'
-                ),
+                birthdate=faker.date_between(start_date='-80y', end_date='-20y'),
                 email=faker.email(),
                 years_of_experience=faker.random_int(min=1, max=60),
-                academic_degrees=random.choice(ACADEMIC_DEGREES)
+                academic_degrees=random.choice(ACADEMIC_DEGREES),
             )
+            tchr.age = relativedelta(datetime.date.today(), tchr.birthdate).years
             tchr.save()
             create_teachers.append(str(tchr))
         return create_teachers
