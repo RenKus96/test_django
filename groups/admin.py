a50d7a49b5f541a07e3214aa3774f649cef60f79
RenKus <79753231+RenKus96@users.noreply.github.com>
2021-08-04 21:58:00 +0300

    HW2_17

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,70 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from .models import Group
+from students.models import Student
+from teachers.models import Teacher
+
+
+class StudentsInlineTable(admin.TabularInline):
+    model = Student
+    fields = [
+        'first_name',
+        'last_name',
+        'birthdate',
+        'email',
+    ]
+
+    readonly_fields = fields
+    show_change_link = True
+    extra = 0
+
+class TeachersInlineTable(admin.TabularInline):
+    model = Teacher
+    fields = [
+        'academic_degrees',
+        'first_name',
+        'last_name',
+        'email',
+    ]
+
+    readonly_fields = fields
+    show_change_link = True
+    extra = 0
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = [
+        'group_number',
+        'course',
+        'date_of_creation',
+        'end_date',
+        'number_of_students',
+        'headman',
+    ]
+    
+    list_display_links = list_display
+    list_per_page = 10
+
+    fields = [
+        ('group_number',
+        'course'),
+        ('date_of_creation',
+        'end_date'),
+        ('number_of_students',
+        'headman'),
+    ]
+
+    readonly_fields = ['course', 'headman']
+
+    search_fields = [
+        'group_number',
+    ]
+
+    list_filter = [
+        'course',
+    ]
+
+    inlines = [StudentsInlineTable, TeachersInlineTable]
+
+admin.site.register(Group, GroupAdmin)
--- lms/settings.py
+++ lms/settings.py
@@ -52,13 +52,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'core.middlewares.SimpleMiddleware',
+
 ]
 
 ROOT_URLCONF = 'lms.urls'
